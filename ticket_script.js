78b445bc2ba5049f1be22b23512ffc09bff6cf44
andrea-scm <a.scimitto@gmail.com>
2019-04-16 14:01:31 +0200

    done

---
+++ ticket_script.js
@@ -0,0 +1,29 @@
+var dati, frase,prezzo,sconto;
+dati = [];
+frase = '';
+
+for (n = 1; n >= 0; n-- ){
+  if(n == 1){
+    frase = 'inserisci km';
+  }else{
+    frase = 'inserisci età';
+  }
+  dati[n] = prompt(frase);
+  console.log(dati);
+}
+
+prezzo = dati[1] * 0.21;
+
+if (dati[0] < 18){
+  sconto = (prezzo*20)/100;
+  prezzo= prezzo - sconto;
+}else if(dati[0] >59){
+  sconto = (prezzo*60)/100;
+  prezzo = prezzo - sconto;
+}else{
+  prezzo = prezzo;
+}
+
+document.getElementById('età').innerHTML = dati[0];
+document.getElementById('km').innerHTML = dati[1];
+document.getElementById('prezzo').innerHTML = prezzo + '€';
